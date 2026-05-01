c32ea2d7997a7f368ea9ffde8b3bbc3f31017cb7
DARPAN BANERJEE <banerjeedarpan2000@gmail.com>
2026-05-01 21:42:32 +0530

    Implement typing effect for multiple texts

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const text = "Hi, I'm Darpan Banerjee";
-let i = 0;
+const texts = ["Hi, I'm Darpan Banerjee", "Data Analyst", "Python Developer"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
 
-function typeEffect() {
-  if (i < text.length) {
-    document.getElementById("typing").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typeEffect, 50);
+function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById('typing').textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1000);
+  } else {
+    setTimeout(type, 50);
   }
 }
 
-typeEffect();
+type();
